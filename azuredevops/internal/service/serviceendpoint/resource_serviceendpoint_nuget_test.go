444b6c00515d5d6c323ffd322fcdf848e72bb5bd
Chethan Nagaraja <chethan.nagaraja@ni.com>
2023-03-20 10:49:28 +0530

    Created service end point for NuGet

---
+++ azuredevops/internal/service/serviceendpoint/resource_serviceendpoint_nuget_test.go
@@ -0,0 +1,163 @@
+//go:build (all || resource_serviceendpoint_nuget) && !exclude_serviceendpoints
+// +build all resource_serviceendpoint_nuget
+// +build !exclude_serviceendpoints
+
+package serviceendpoint
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/google/uuid"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/microsoft/azure-devops-go-api/azuredevops/v6/serviceendpoint"
+	"github.com/microsoft/terraform-provider-azuredevops/azdosdkmocks"
+	"github.com/microsoft/terraform-provider-azuredevops/azuredevops/internal/client"
+	"github.com/microsoft/terraform-provider-azuredevops/azuredevops/internal/utils/converter"
+	"github.com/stretchr/testify/require"
+)
+
+var nugetTestServiceEndpointID = uuid.New()
+var nugetRandomServiceEndpointProjectID = uuid.New()
+var nugetTestServiceEndpointProjectID = &nugetRandomServiceEndpointProjectID
+
+var nugetTestServiceEndpoint = serviceendpoint.ServiceEndpoint{
+	Authorization: &serviceendpoint.EndpointAuthorization{
+		Parameters: &map[string]string{
+			"apitoken": "NUGET_TEST_access_token",
+		},
+		Scheme: converter.String("Token"),
+	},
+	Id:    &nugetTestServiceEndpointID,
+	Name:  converter.String("UNIT_TEST_CONN_NAME"),
+	Owner: converter.String("library"),
+	Type:  converter.String("externalnugetfeed"),
+	Url:   converter.String("https://api.nuget.org/v3/index.json"),
+	ServiceEndpointProjectReferences: &[]serviceendpoint.ServiceEndpointProjectReference{
+		{
+			ProjectReference: &serviceendpoint.ProjectReference{
+				Id: nugetTestServiceEndpointProjectID,
+			},
+			Name:        converter.String("UNIT_TEST_CONN_NAME"),
+			Description: converter.String("UNIT_TEST_CONN_DESCRIPTION"),
+		},
+	},
+}
+
+// verifies that the flatten/expand round trip yields the same service endpoint
+func TestServiceEndpointNuget_ExpandFlatten_Roundtrip(t *testing.T) {
+	resourceData := schema.TestResourceDataRaw(t, ResourceServiceEndpointNuget().Schema, nil)
+	flattenServiceEndpointNuget(resourceData, &nugetTestServiceEndpoint, nugetTestServiceEndpointProjectID)
+
+	serviceEndpointAfterRoundTrip, projectID, err := expandServiceEndpointNuget(resourceData)
+
+	require.Equal(t, nugetTestServiceEndpoint, *serviceEndpointAfterRoundTrip)
+	require.Equal(t, nugetTestServiceEndpointProjectID, projectID)
+	require.Nil(t, err)
+}
+
+// verifies that if an error is produced on create, the error is not swallowed
+func TestServiceEndpointNuget_Create_DoesNotSwallowError(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	r := ResourceServiceEndpointNuget()
+	resourceData := schema.TestResourceDataRaw(t, r.Schema, nil)
+	flattenServiceEndpointNuget(resourceData, &nugetTestServiceEndpoint, nugetTestServiceEndpointProjectID)
+
+	buildClient := azdosdkmocks.NewMockServiceendpointClient(ctrl)
+	clients := &client.AggregatedClient{ServiceEndpointClient: buildClient, Ctx: context.Background()}
+
+	expectedArgs := serviceendpoint.CreateServiceEndpointArgs{Endpoint: &nugetTestServiceEndpoint}
+	buildClient.
+		EXPECT().
+		CreateServiceEndpoint(clients.Ctx, expectedArgs).
+		Return(nil, errors.New("CreateServiceEndpoint() Failed")).
+		Times(1)
+
+	err := r.Create(resourceData, clients)
+	require.Contains(t, err.Error(), "CreateServiceEndpoint() Failed")
+}
+
+// verifies that if an error is produced on a read, it is not swallowed
+func TestServiceEndpointNuget_Read_DoesNotSwallowError(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	r := ResourceServiceEndpointNuget()
+	resourceData := schema.TestResourceDataRaw(t, r.Schema, nil)
+	flattenServiceEndpointNuget(resourceData, &nugetTestServiceEndpoint, nugetTestServiceEndpointProjectID)
+
+	buildClient := azdosdkmocks.NewMockServiceendpointClient(ctrl)
+	clients := &client.AggregatedClient{ServiceEndpointClient: buildClient, Ctx: context.Background()}
+
+	expectedArgs := serviceendpoint.GetServiceEndpointDetailsArgs{
+		EndpointId: nugetTestServiceEndpoint.Id,
+		Project:    converter.String(nugetTestServiceEndpointProjectID.String()),
+	}
+	buildClient.
+		EXPECT().
+		GetServiceEndpointDetails(clients.Ctx, expectedArgs).
+		Return(nil, errors.New("GetServiceEndpoint() Failed")).
+		Times(1)
+
+	err := r.Read(resourceData, clients)
+	require.Contains(t, err.Error(), "GetServiceEndpoint() Failed")
+}
+
+// verifies that if an error is produced on a delete, it is not swallowed
+func TestServiceEndpointNuget_Delete_DoesNotSwallowError(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	r := ResourceServiceEndpointNuget()
+	resourceData := schema.TestResourceDataRaw(t, r.Schema, nil)
+	flattenServiceEndpointNuget(resourceData, &nugetTestServiceEndpoint, nugetTestServiceEndpointProjectID)
+
+	buildClient := azdosdkmocks.NewMockServiceendpointClient(ctrl)
+	clients := &client.AggregatedClient{ServiceEndpointClient: buildClient, Ctx: context.Background()}
+
+	expectedArgs := serviceendpoint.DeleteServiceEndpointArgs{
+		EndpointId: nugetTestServiceEndpoint.Id,
+		ProjectIds: &[]string{
+			nugetTestServiceEndpointProjectID.String(),
+		},
+	}
+	buildClient.
+		EXPECT().
+		DeleteServiceEndpoint(clients.Ctx, expectedArgs).
+		Return(errors.New("DeleteServiceEndpoint() Failed")).
+		Times(1)
+
+	err := r.Delete(resourceData, clients)
+	require.Contains(t, err.Error(), "DeleteServiceEndpoint() Failed")
+}
+
+// verifies that if an error is produced on an update, it is not swallowed
+func TestServiceEndpointNuget_Update_DoesNotSwallowError(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	r := ResourceServiceEndpointNuget()
+	resourceData := schema.TestResourceDataRaw(t, r.Schema, nil)
+	flattenServiceEndpointNuget(resourceData, &nugetTestServiceEndpoint, nugetTestServiceEndpointProjectID)
+
+	buildClient := azdosdkmocks.NewMockServiceendpointClient(ctrl)
+	clients := &client.AggregatedClient{ServiceEndpointClient: buildClient, Ctx: context.Background()}
+
+	expectedArgs := serviceendpoint.UpdateServiceEndpointArgs{
+		Endpoint:   &nugetTestServiceEndpoint,
+		EndpointId: nugetTestServiceEndpoint.Id,
+	}
+
+	buildClient.
+		EXPECT().
+		UpdateServiceEndpoint(clients.Ctx, expectedArgs).
+		Return(nil, errors.New("UpdateServiceEndpoint() Failed")).
+		Times(1)
+
+	err := r.Update(resourceData, clients)
+	require.Contains(t, err.Error(), "UpdateServiceEndpoint() Failed")
+}
